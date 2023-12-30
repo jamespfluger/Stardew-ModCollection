df96e8e05a37fbddcf94507e8173d1e430fa60f8
James Pfluger <me@jamespfluger.com>
2023-12-30 09:53:15 -0800

    update some mods to .NET 5

---
+++ StardewSandbox/Sandbox.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Sandbox : Mod
+    {
+        StardewValley.Object currentObject = null;
+        bool shouldDrawTooltip = false;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Player.Warped += OnWarpedHandler;
+        }
+
+        private void OnWarpedHandler(object sender, WarpedEventArgs args)
+        {
+
+        }
+
+    }
+}
