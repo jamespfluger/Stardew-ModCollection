5aa8a480b7e300292cc8b40299bffa12c89a8ddf
James Pfluger <me@jamespfluger.com>
2023-12-30 17:41:18 -0800

    make relaxing weekends

---
+++ RelaxingWeekends/RelaxingWeekendsMod.cs
@@ -0,0 +1,58 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using StardewValley.TerrainFeatures;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AutoWeekendWatering
+{
+    /// <summary>
+    /// Mod for watering all your crops on the weekend
+    /// </summary>
+    public class RelaxingWeekendsMod : Mod
+    {
+        /// <inheritdoc />
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+        }
+
+        /// <summary>
+        /// Automatically waters all tilled dirt
+        /// </summary>
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            Game1.addMailForTomorrow($"This mail was sent via a string", noLetter: false, sendToEveryone: false);
+            // Only run on the weekends
+            if (Game1.Date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday)
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+
+                foreach (GameLocation location in Game1.locations)
+                {
+                    // Each location has terrain features, and some may be able to grow plants
+                    foreach (TerrainFeature terrainFeature in location.terrainFeatures.Values)
+                    {
+                        if (terrainFeature is HoeDirt hoedirt)
+                        {
+                            // Simple setting its value to 1 marks it as watered
+                            hoedirt.state.Value = 1;
+                        }
+                    }
+
+                    // Pots can be indside buildings as well
+                    foreach (IndoorPot indoorPot in location.objects.Values.OfType<IndoorPot>())
+                    {
+                        indoorPot.showNextIndex.Value = true;
+                    }
+                }
+                Monitor.Log($"Time to water: {sw.Elapsed}");
+            }
+        }
+    }
+}
